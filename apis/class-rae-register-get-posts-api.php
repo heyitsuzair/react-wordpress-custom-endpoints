265c0244b214dede0d4afca84aacfd14a9313d38
heyitsuzair <uzairdevil354123@gmail.com>
2022-11-12 02:02:32 +0500

    Posts Retrieve From Endpoint Added

--- apis/class-rae-register-get-posts-api.php
+++ apis/class-rae-register-get-posts-api.php
@@ -57,13 +57,44 @@ class Rae_Register_Get_Posts_Api
             'update_post_meta_cache' => false,
             'update_post_term_cache' => false
         ];
-        $latest_post = new WP_Query($args);
-
-        $post_result = $latest_post->posts;
+        $latest_post_ids = new WP_Query($args);
+        $post_result = $this->get_required_posts_data($latest_post_ids->posts);
 
         return [
             'post_data' => $post_result
         ];
+    }
+    public function get_required_posts_data($post_IDs)
+    {
+        $post_result = [];
+
+        if (empty($post_IDs) && !is_array($post_IDs)) {
+            return $post_result;
+        }
+
+        foreach ($post_IDs as $post_ID) {
+            $author_id = get_post_field('post_author', $post_ID);
+            $thumbnail_id = get_post_thumbnail_id($post_ID);
+
+            $post_data = [];
+            $post_data['id'] = $post_ID;
+            $post_data['title'] = get_the_title($post_ID);
+            $post_data['excerpt'] = get_the_excerpt($post_ID);
+            $post_data['date'] = get_the_date('', $post_ID);
+            $post_data['attachement_image'] = [
+                'img_sizes' => wp_get_attachment_image_sizes($thumbnail_id),
+                'img_src' => wp_get_attachment_image_src($thumbnail_id, 'full'),
+                'img_srcset' => wp_get_attachment_image_srcset($thumbnail_id)
+            ];
+            $post_data['categories'] = get_the_category($post_ID);
+            $post_data['meta'] = [
+                'author_id' => $author_id,
+                'author_name' => get_the_author_meta('display_name', $author_id)
+            ];
+
+            array_push($post_result, $post_data);
+        }
+        return $post_result;
     }
 }
 new Rae_Register_Get_Posts_Api();
